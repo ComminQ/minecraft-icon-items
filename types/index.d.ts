78d9d94408f80ab41db1bd6e1d818c6c4f87930d
Fabien CAYRE <fabiencayre81@gmail.com>
2022-09-06 22:01:33 +0300

    Added typing

---
+++ types/index.d.ts
@@ -0,0 +1,16 @@
+declare module "minecraft-icon-items" {
+  export function get(key: string): ItemIcon | null;
+  export function getBukkit(key: string): ItemIcon | null;
+  export function getAll(by: "name" | "id" = "id"): Map<String, ItemIcon>;
+}
+
+interface ItemIcon {
+  id: string;
+  name: string;
+  meta: number;
+  type: number;
+  /**
+   * Base 64 encoded img (32x32 pixels)
+   */
+  icon: string;
+}
